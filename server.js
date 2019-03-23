352c1952deb8066d1c491254fbe549049fcc42e1
aiman <aiman.younis4@gmail.com>
2019-03-23 21:44:36 +0200

    port changed

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use('/',serveStatic(path.join(__dirname,'/dist')));
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port);
 console.log(`listening on port: ${port}`);
